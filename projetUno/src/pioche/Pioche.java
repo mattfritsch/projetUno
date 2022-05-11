3ad44a1eb33ed9e3f14c9dc1c786fc25af4a9f82
Matthieu <mattfritsch98380@gmail.com>
2022-05-11 11:14:37 +0200

    correction de tas et pioche +ajout de commentaire pour nathan

--- projetUno/src/pioche/Pioche.java
+++ projetUno/src/pioche/Pioche.java
@@ -1,52 +1,51 @@
 package pioche;
 
-import java.util.Arraylist;
+import java.util.ArrayList;
+
+import carte.Carte;
 import exception.PiocheException;
 
 public class Pioche {
-	
-	private:
-		
-		public ArrayList<Carte> pioche;
-	
-		public Pioche() {
-			
+
+	public ArrayList<Carte> pioche;
+
+	public Pioche() {
+
+	}
+
+	public void addCarte(Carte carte) {
+		pioche.add(carte);
+	}
+
+	public void removeCarte(int index) {
+		pioche.remove(index);
+	}
+
+	public int getSize() {
+		return pioche.size();
+	}
+
+	public Carte getTop() throws PiocheException {
+		if (pioche.size() > 0) {
+			return pioche.get(getSize() - 1);
+		} else {
+			throw new PiocheException("Pioche vide");
 		}
-		
-		public void addCarte(Carte carte) {
-			pioche.add(carte);
-		}
-		
-		public void removeCarte(int index) {
-			pioche.remove(index);
-		}
-		
-		public int getSize() {
-			return size(pioche);
-		}
-		
-		public Carte getTop() throws PiocheException{
-			if (pioche.getSize()>0) {
-				return pioche[pioche.getSize()-1];
-			}
-			else {
-				throw new PiocheException("Pioche vide");
-			}
-		}
-		
-		public ArrayList<Carte> piocher(int nbCartes) throws PiocheException{
-			ArrayList<Carte> cartesPiochees;
-			if (nbCartes<=pioche.getSize(){
+	}
+
+	public ArrayList<Carte> piocher(int nbCartes) throws PiocheException{
+			ArrayList<Carte> cartesPiochees = new ArrayList<Carte>();
+			if (nbCartes<=getSize()){
 				for (int i=0; i<nbCartes; i++) {
-					Carte carte = pioche.getTop();
+					Carte carte = getTop();
 					cartesPiochees.add(carte);
-					pioche.removeCarte(pioche.getSize()-1);
+					removeCarte(getSize() - 1);
 				}
 			}
 			else {
 				throw new PiocheException("TODO");
 			}
 			
-			return cartePiochees;
+			return cartesPiochees;
 		}
 }
--- projetUno/src/tas/Tas.java
+++ projetUno/src/tas/Tas.java
@@ -1,51 +1,62 @@
 package tas;
 
-import java.util.Arraylist;
+import java.util.ArrayList;
 import java.util.Collections;
+
+import carte.Carte;
 import exception.TasException;
+import pioche.Pioche;
+
+/*
+ * Je sais pas si les primitives que tu as faites sont vraiment utile
+ * -> addCarte, removeCarte, getSize, getTop... 
+ * Car tu peux tout faire directement dans la fonction concernée
+ * A toi de voir, car ça peut allourdir le code même si ça peut peut-être facilité sa compréhension
+ * Même commentaire pour pioche
+ * */
 
 public class Tas {
-	
-	private:
-		
-		private ArrayList<Carte> tas;
-	
-		public Tas () {
-			
+
+	public ArrayList<Carte> tas;
+
+	public Tas() {
+
+	}
+
+	public void addCarte(Carte carte) {
+		tas.add(carte);
+	}
+
+	/*
+	 * Possiblement a ?
+	 * Changer par index ou par carte
+	 * */
+
+	public void removeCarte(int index) {
+		tas.remove(index);
+	}
+
+	public int getSize() {
+		return tas.size();
+	}
+
+	public Carte getTop() {
+		if (getSize() > 0) {
+			return tas.get(getSize()-1);
+		} else {
+			return null;
 		}
-		
-		
-		public void addCarte(Carte carte) {
-			tas.add(carte);
+	}
+
+	public Pioche melangerTas() {
+		Carte derniereCarte = getTop();
+		// removeCarte(derniereCarte); -> fonctionne pas car tu passes une carte en paramètre et removeCarte prend un int
+		Collections.shuffle(tas);
+		//Pioche pioche = (Pioche) tas; -> Tu peux pas cast une arrayList
+		for (Carte carte : tas) {
+			//removeCarte(getTop()); -> pareil que pour la ligne 53
 		}
-		
-		##Possiblement a changer (par index ou par carte)
-		public void removeCarte(int index) {
-			tas.remove(index);
-		}
-		
-		public int getSize() {
-			return size(tas);
-		}
-		
-		public Carte getTop() {
-			if (tas.getSize()>0) {
-				return tas[tas.getSize()-1];
-			}
-			else {
-				return null;
-			}
-		}
-		
-		public Pioche melangerTas() {
-			Carte derniereCarte = tas.getTop();
-			tas.removeCarte(derniereCarte);
-			Collections.shuffle(tas);
-			Pioche pioche = (Pioche) tas;
-			for (Carte carte: tas) {
-				tas.removeCarte(tas.getTop());
-			}
-			tas.addCarte(derniereCarte);
-			return pioche;
-		}
+		/* addCarte(derniereCarte) ne fonctionne pas*/tas.add(derniereCarte);
+		//return pioche;
+	}
 }
