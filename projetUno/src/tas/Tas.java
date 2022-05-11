491a426a26e928edc8b14588a0f0838469acbb00
nat <nat@nat-VirtualBox>
2022-05-11 10:22:25 +0200

    Pioche et Tas

--- projetUno/src/tas/Tas.java
+++ projetUno/src/tas/Tas.java
@@ -1,5 +1,51 @@
 package tas;
 
-public class Tas {
+import java.util.Arraylist;
+import java.util.Collections;
+import exception.TasException;
 
+public class Tas {
+	
+	private:
+		
+		private ArrayList<Carte> tas;
+	
+		public Tas () {
+			
+		}
+		
+		
+		public void addCarte(Carte carte) {
+			tas.add(carte);
+		}
+		
+		##Possiblement a changer (par index ou par carte)
+		public void removeCarte(int index) {
+			tas.remove(index);
+		}
+		
+		public int getSize() {
+			return size(tas);
+		}
+		
+		public Carte getTop() {
+			if (tas.getSize()>0) {
+				return tas[tas.getSize()-1];
+			}
+			else {
+				return null;
+			}
+		}
+		
+		public Pioche melangerTas() {
+			Carte derniereCarte = tas.getTop();
+			tas.removeCarte(derniereCarte);
+			Collections.shuffle(tas);
+			Pioche pioche = (Pioche) tas;
+			for (Carte carte: tas) {
+				tas.removeCarte(tas.getTop());
+			}
+			tas.addCarte(derniereCarte);
+			return pioche;
+		}
 }
