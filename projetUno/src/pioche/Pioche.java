f49c61e6babc7948a32f1a604a82f0c5b258d3b1
nat <nat@nat-VirtualBox>
2022-05-14 15:14:17 +0200

    Méthodes standardes Pioche

--- projetUno/src/pioche/Pioche.java
+++ projetUno/src/pioche/Pioche.java
@@ -1,8 +1,10 @@
 package pioche;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 import carte.Carte;
+import carte.CarteChangement;
 import exception.PiocheException;
 
 public class Pioche {
@@ -33,18 +35,43 @@ public class Pioche {
 	}
 
 	public ArrayList<Carte> piocher(int nbCartes) throws PiocheException{
-			ArrayList<Carte> cartesPiochees = new ArrayList<Carte>();
-			if (nbCartes<=getSize()){
-				for (int i=0; i<nbCartes; i++) {
-					Carte carte = getTop();
-					cartesPiochees.add(carte);
-					this.removeCarte(this.getTop());
-				}
+		ArrayList<Carte> cartesPiochees = new ArrayList<Carte>();
+		if (nbCartes<=getSize()){
+			for (int i=0; i<nbCartes; i++) {
+				Carte carte = getTop();
+				cartesPiochees.add(carte);
+				this.removeCarte(this.getTop());
 			}
-			else {
-				throw new PiocheException("TODO");
-			}
-			
-			return cartesPiochees;
 		}
+		else {
+			throw new PiocheException("TODO");
+		}
+			
+		return cartesPiochees;
+	}
+	
+	@Override
+	public String toString() {
+		String ch = "Pioche [";
+		for (Carte carte : pioche) {
+			ch+=carte.toString();
+		}
+		return ch + "]";
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(pioche);
+	}
+	
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this==obj) return true;
+		if (obj==null) return false;
+		if (getClass() != obj.getClass()) return false;
+		
+		Pioche other = (Pioche) obj;
+		return pioche == other.pioche;
+	}
 }
